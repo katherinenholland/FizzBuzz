1d5f50579cf9d741b73091eb78ecd5868214f7dc
katherinenholland <katherinenholland@gmail.com>
2014-04-01 10:12:21 -0400

    First Attempt

---
+++ app.js
@@ -0,0 +1,18 @@
+
+for (var i = 1; i <= 100; i++) {
+	alert(i);
+	if (i % 3 == 0) {
+    alert("fizz");
+	} else if(i % 5 == 0) {
+	alert("buzz");
+	} else if (i % 5 == 0, i % 3 == 0)
+	alert("fizz buzz"); 
+};
+
+
+
+
+
+
+
+ 
