bb1fe2a9ed3174aff1e01fd070d2e6a8634ce562
katherinenholland <katherinenholland@gmail.com>
2014-04-01 10:47:48 -0400

    Working Fizz Buzz

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 
-for (var i = 1; i <= 100; i++) {
-	alert(i);
-	if (i % 3 == 0) {
+var count = [];
+
+for (var i = 1; i <= 15; i++) {
+	if (i % 5 == 0 && i % 3 == 0) {
+	alert("fizz buzz");
+	count.push("fizz buzz");
+	} else if(i % 3 == 0) {
     alert("fizz");
+    count.push("fizz");
 	} else if(i % 5 == 0) {
 	alert("buzz");
-	} else if (i % 5 == 0, i % 3 == 0)
-	alert("fizz buzz"); 
+	count.push("buzz");
+    } else if(i % 3 !== 0, i % 5 !== 0) 
+	alert(i);
+	count.push(i);
 };
 
-
+document.write(count);
 
 
 
