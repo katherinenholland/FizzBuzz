835f81be94a328846437bcc0f27df0e2b9f26de2
katherinenholland <katherinenholland@gmail.com>
2014-04-01 10:54:27 -0400

    Working Fizz Buzz with no alerts

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
 
 var count = [];
 
-for (var i = 1; i <= 15; i++) {
+for (var i = 1; i <= 100; i++) {
 	if (i % 5 == 0 && i % 3 == 0) {
-	alert("fizz buzz");
 	count.push("fizz buzz");
 	} else if(i % 3 == 0) {
-    alert("fizz");
     count.push("fizz");
 	} else if(i % 5 == 0) {
-	alert("buzz");
 	count.push("buzz");
     } else if(i % 3 !== 0, i % 5 !== 0) 
-	alert(i);
 	count.push(i);
 };
 
